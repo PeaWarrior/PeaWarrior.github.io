cd3592aca26561b3578156b216d5b00710c6bd04
PeaWarrior <jacksonchen171@gmail.com>
2020-11-09 22:28:49 -0500

    Updates

---
+++ component---src-pages-404-js-19a8b431d21d3458526f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),l=t.n(a);e.default=function(){return l.a.createElement("div",null,l.a.createElement("h1",null,"404"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-19a8b431d21d3458526f.js.map
